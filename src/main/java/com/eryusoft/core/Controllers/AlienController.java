60ed1b627b543918abbc8aa9a7f534acd88d33d7
ericpinera <eepinera.emp@gmail.com>
2019-07-03 12:00:26 -0500

    DAO y REPO

--- src/main/java/com/eryusoft/core/Controllers/AlienController.java
+++ src/main/java/com/eryusoft/core/Controllers/AlienController.java
@@ -1,14 +1,28 @@
 package com.eryusoft.core.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eryusoft.core.dao.AlienRepo;
+import com.eryusoft.core.model.Alien;
 
 @Controller
 public class AlienController {
 	
+	@Autowired
+	AlienRepo repo;
+	
 	@RequestMapping("/")
 	public String home()
 	{
+		return "Home.html";
+	}
+	
+	@RequestMapping("/addAlien")
+	public String addAlien(Alien alien)
+	{
+		repo.save(alien);
 		return "Home.html";
 	}
 
