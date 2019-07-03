d59ea6121d0649bea99c912990c3a2551782aa7e
ericpinera <eepinera.emp@gmail.com>
2019-07-03 08:17:48 -0500

    data.sql

--- src/main/java/com/eryusoft/core/model/Alien.java
+++ src/main/java/com/eryusoft/core/model/Alien.java
@@ -1,7 +1,12 @@
 package com.eryusoft.core.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Alien {
 	
+	@Id
 	private int aid;	
 	private String aname;
 	public int getAid() {
